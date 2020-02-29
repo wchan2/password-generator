e1d92ecd602126748c2cd877bf24fd97c4615de3
Will <willchan2@gmail.com>
2020-02-29 17:32:49 -0500

    initial version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const generatorFunction = (chars, length = 8) => {
+  return () => {
+    const passwordArr = new Array(length);
+    for (let i = 0; i < passwordArr.length; i++) {
+      const randCharIndex = Math.floor(Math.random() * passwordArr.length);
+      passwordArr[i] = chars[randCharIndex];
+    }
+    return passwordArr.join('');
+  };
+};
 
-export default App;
+const PasswordInput = (props) => {
+  return (
+    <input
+      type={props.visible ? 'text' : 'password'}
+      name={props.name}
+      value={props.value}
+      onChange={props.onChange} />
+  );
+};
+
+class PasswordGenerator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: '',
+      showPassword: false,
+    };
+
+    this.generatePassword = this.generatePassword.bind(this);
+    this.togglePasswordVisibility = this.togglePasswordVisibility.bind(this);
+  }
+
+  generatePassword() {
+    this.setState({ password: this.props.generatorFunction() });
+  }
+
+  togglePasswordVisibility() {
+    this.setState({ showPassword: !this.state.showPassword });
+  }
+
+  render() {
+    return (
+      <div>
+        <PasswordInput visible={this.state.showPassword} name="password" value={this.state.password} onChange={this.generatePassword} />
+        <input type="button" value="Generate Password" onClick={this.generatePassword} />
+        <input
+          type="button"
+          value={!this.state.showPassword ? 'Show Password' : 'Hide Password'}
+          onClick={this.togglePasswordVisibility}
+        />
+      </div>
+    );
+  }
+};
+
+export default function App() {
+  return <PasswordGenerator generatorFunction={generatorFunction('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 12)} />
+}
